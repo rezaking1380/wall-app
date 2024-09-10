3a9e08a9c5129c4aeda386395b9b09dce556a5ec
reza <rezanangir9@gmail.com>
2024-09-10 21:04:25 +0330

    fix for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import AddNoteForm from "@/components/AddNoteForm";
 import Wall from "@/components/Wall";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Wall />
-      {/* <AddNoteForm /> */}
     </main>
   );
 }
